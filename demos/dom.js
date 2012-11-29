8e9b03b288e332330fe2f0057e3756bf80b4ccc5
Rob Ashton <robashton@codeofrob.com>
2012-11-29 11:09:38 +0000

    slow particles

---
+++ demos/dom.js
@@ -0,0 +1,7 @@
+var ele = document.getElementById('foo')
+ele.style.position = 'absolute'
+ele.style.top = '0'
+ele.style.left = '0'
+ele.width = '100px'
+ele.height = '100px'
+ele.style.background.color = '#F00'
